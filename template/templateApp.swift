0379e71cad7cb8415e2bf1defb2df236795f51fd
Senthilkumar Maruthasalam <senthilinfo62@gmail.com>
2025-06-11 09:23:05 +0530

    Initial Commit

---
+++ template/templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  templateApp.swift
+//  template
+//
+//  Created by Senthilkumar Maruthasalam on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ templateTests/templateTests.swift
@@ -0,0 +1,16 @@
+//
+//  templateTests.swift
+//  templateTests
+//
+//  Created by Senthilkumar Maruthasalam on 11/06/25.
+//
+
+import Testing
+
+struct templateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
